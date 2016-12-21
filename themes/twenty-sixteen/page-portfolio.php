0f40927fc90678004120ab91efee9b742a34bfff
Vincent Ragosta <vragosta@storycorps.org>
2016-12-21 17:14:28 -0500

    continued work on portfolio

--- themes/twenty-sixteen/page-portfolio.php
+++ themes/twenty-sixteen/page-portfolio.php
@@ -8,13 +8,33 @@
 
 get_header();
 
-$content = $post->post_content;
+$args = array(
+	'post_type' => 'project'
+);
 
-?>
+$projects = new WP_Query( $args ); ?>
 
-<main class="portfolio">
+<main id="portfolio">
 	<section class="sidebar cta a flex-center">
-		<?php echo $content; ?>
+		<?php echo $post->post_content; ?>
+	</section>
+
+	<section>
+		<?php if ( $projects->have_posts() ) : ?>
+			<?php while ( $projects->have_posts() ) : $projects->the_post(); ?>
+				<?php $image = vincentragosta_com\Twenty_Sixteen\Helpers\vr_get_featured_image( $post->ID ); ?>
+				<div class="col-xs-12 col-sm-4">
+					<div class="featured-image aspect-ratio-1x1">
+						<div class="overlay flex-center not-visible">
+							<span class="sub-title padding-left-right">Wordpress Site</span>
+							<span class="title padding-left-right"><?php echo esc_html( $post->post_title ); ?></span>
+						</div>
+						<div class="post-type normalize-image" style="background-image: url( '<?php echo esc_attr( $image ); ?>' );"></div>
+					</div>
+				</div>
+			<?php endwhile; ?>
+			<?php wp_reset_postdata(); ?>
+		<?php endif; ?>
 	</section>
 </main>
 
