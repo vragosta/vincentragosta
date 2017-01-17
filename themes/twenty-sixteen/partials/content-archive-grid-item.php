5d5e49c6e0ccd7b55396d0a32c0637e408c9e866
Vincent Ragosta <allchenzo@gmail.com>
2017-01-16 19:21:49 -0500

    removed comments from theme css file, css fix to sub headers, resolved issue on archive template

--- themes/twenty-sixteen/partials/content-archive-grid-item.php
+++ themes/twenty-sixteen/partials/content-archive-grid-item.php
@@ -4,16 +4,13 @@
  *
  * @package VincentRagosta 2016
  * @since   0.1.0
- * @uses    get_post_meta(), vr_get_featured_image(), esc_html(), get_the_parmalink()
+ * @uses    do_shortcode()
  */
-
-// Get the featured-image from the current post.
-$image = vincentragosta_com\Twenty_Sixteen\Helpers\vr_get_featured_image( $post->ID ); ?>
+?>
 
 <div class="grid-item col-xs-12 col-sm-4">
 
 	<!-- Featured image overlay -->
-	<?php // include( locate_template( 'partials/content-featured-image-overlay.php', false, false ) ); ?>
 	<?php do_shortcode( '[image-caption id="' . $post->ID . '"]' ); ?>
 
 </div>
