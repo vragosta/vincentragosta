6cb6e3cef914533b3c12b2676afb2649eb2d80b3
Vincent Ragosta <vragosta@storycorps.org>
2017-08-21 10:49:36 -0400

    added page and post types

--- plugins/vincentragosta/includes/VincentRagosta/PostTypes/PostPostType.php
+++ plugins/vincentragosta/includes/VincentRagosta/PostTypes/PostPostType.php
@@ -22,10 +22,5 @@ class PostPostType extends BasePostType {
 	public function register_post_type() {
 		add_post_type_support( 'post', 'postmeta-fields' );
 		remove_post_type_support( 'post', 'custom-fields' );
-		// $post_type = get_post_type_object( $this->get_name() );
-		// $post_type->show_ui = current_user_can( 'manage_posts' );
-		// register_post_type(
-		// 	$this->get_name(), (array) $post_type
-		// );
 	}
 }
