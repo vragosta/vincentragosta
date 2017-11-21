0e73276db10b9c65fab05a5ee2f18261711ce7b7
Vincent Ragosta <vincentpasqualeragosta@gmail.com>
2017-11-20 21:48:22 -0500

    added uglify build and dev tasks, added watch task

--- themes/twenty-sixteen/Gruntfile.js
+++ themes/twenty-sixteen/Gruntfile.js
@@ -5,16 +5,34 @@ module.exports = function (grunt) {
 		pkg: grunt.file.readJSON( 'package.json' ),
 		uglify: {
 			build: {
-				src: 'src/js/*.js',
-				dest: ''
+				src: 'assets/js/src/vincentragosta---twenty-sixteen.js',
+				dest: 'assets/js/vincentragosta---twenty-sixteen.min.js'
+			},
+			dev: {
+				options: {
+					beautify: true,
+					mangle: false,
+					compress: false,
+					preserveComments: 'all'
+				},
+				src: 'assets/js/src/vincentragosta---twenty-sixteen.js',
+				dest: 'assets/js/vincentragosta---twenty-sixteen.min.js'
+			}
+		},
+		watch: {
+			js: {
+				files: [ 'assets/js/src/vincentragosta---twenty-sixteen.js' ],
+				tasks: [ 'uglify:dev' ]
 			}
 		}
 	});
 
 	// Load the plugins
-	grunt.loadNpmTasks();
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
 	// Register task(s)
-	grunt.registerTask( 'default', [] );
+	grunt.registerTask( 'default', [ 'uglify:dev' ] );
+	grunt.registerTask( 'build', [ 'uglify:build' ] );
 
 };
